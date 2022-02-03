b5cbc23217ae475d48f0cec87315a09bd42732d4
Edwin Fame <edwinfame@yahoo.com>
2022-02-02 20:00:46 -0800

    recipe cards appended based on user choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Define all DOM elements here
 const testBtn = document.querySelector('.test-api')
+const recipeResults = document.querySelector('.recipe-results')
 
 //API INFO HERE
 const APIKEY = '5f7f6407a3df426fb065f2211ab36e41'
@@ -12,26 +13,38 @@ function getRecipe() {
     var ingredient = document.querySelector('.ing1').innerText
     var recipeURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=`
     + ingredient 
-    + `&number=3&apiKey=`
+    + `&number=3&ignorePantry&apiKey=`
     + APIKEY;
 
-    // console.log(recipeURL)
+    console.log(recipeURL)
     fetch(recipeURL)
     .then((response) => response.json())
     // .then((data) => console.log(data))
-    .then((data) => showRecipes(data))
-
+    .then((data) => generateRecipe(data))
 }
 
-function showRecipes(data) {
-    let { title, image } = data[1]
-    let usedIng = data[1].usedIngredients[0].name
-    // console.log(usedIng)
-    console.log(title)
-    console.log(image)
-    document.querySelector('.r1img').src = image;
-    document.querySelector('.r1card-title').textContent = title;
-    document.querySelector('.r1currentRecipeIngredient').textContent = usedIng;
 
+function generateRecipe(data){
+    let generatedRecipe = '';
+    data.map(result => {
+        generatedRecipe += 
+        `
+        <div class="col s3 m4">
+        <div class="card r1">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="img activator" src="${result.image}" alt="A picture of generated recipe">
+          </div>
+          <div class="card-content">
+            <span class="card card-title activator grey-text text-darken-4">${result.title}<i class="material-icons right"></i></span>
+            <p><a href="#" data=${result.id}">Link to Recipe</a></p>
+          </div>
+          <div class="card-reveal center">
+            <span class="r1card card-title grey-text text-darken-4">${result.title}<i class="material-icons right">X</i></span>
+            <p class="currentRecipeIngredient">Missing Ingredients:<span class="missingIng">${result.missedIngredientCount}</span></p>
+          </div>
+        </div>
+    </div>
+     `  
+    })
+    recipeResults.innerHTML = generatedRecipe;
 }
-
