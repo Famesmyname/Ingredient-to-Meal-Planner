9741bc6c69e8d2446fc5920a61a306e9f8d76c18
Allyson Vitug <allysonvitugg@gmail.com>
2022-02-02 23:09:12 -0800

    updated sidenav

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,15 +51,15 @@ function generateRecipe(data){
     recipeResults.innerHTML = generatedRecipe;
 }
 
-// // Sidenav
-// var instance = M.Sidenav.getInstance(elem);
-// instance.open();
-// instance.close();
+// Sidenav
+var instance = M.Sidenav.getInstance(elem);
+instance.open();
+instance.close();
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems, options);
-//     var collapsibleElem = document.querySelector('.collapsible');
-//     var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+    // var collapsibleElem = document.querySelector('.collapsible');
+    // var collapsibleInstance = M.Collapsible.init(collapsibleElem, {});
+});
         
