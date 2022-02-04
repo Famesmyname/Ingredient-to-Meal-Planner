db5f2cf13e6eda0c3485c358c97f5af8e174b3ed
Edwin Fame <edwinfame@yahoo.com>
2022-02-03 16:50:04 -0800

    merge commits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 // Define all DOM elements here
-const testBtn = document.querySelector('.test-api')
+const getRecipeBtn = document.querySelector('.get-recipes')
 const recipeResults = document.querySelector('.recipe-results')
 const recipeNumber = document.querySelector('#recipe-number')
-// const myIngCheck = document.querySelector('.myIngCheck')
+const myIngCheck = document.querySelector('.myIngCheck')
 
 //API INFO HERE
 const APIKEY = '5f7f6407a3df426fb065f2211ab36e41'
 
 
-testBtn.addEventListener('click', getRecipe)
+getRecipeBtn.addEventListener('click', getRecipe)
 
 function getRecipe() {
 
-    // if (myIngCheck.checked){
-    //     var myIngRank = "&ranking=2"
-    // }
-    // else {var myIngRank = ""}
-    // console.log(myIngRank)
+    if (myIngCheck.checked){
+        var myIngRank = "&ranking=2"
+    }
+    else {var myIngRank = ""}
+    console.log(myIngRank)
 
     var ingredients = document.querySelector('.ing1').innerText + "," + document.querySelector('.ing2').innerText + "," + document.querySelector('.ing3').innerText + ","  + document.querySelector('.ing4').innerText + ","
     console.log(ingredients)
     var recipeURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=`
     + ingredients 
-    // + myIngRank
+    + myIngRank
     + `&number=`
     + recipeNumber.value
     + `&ignorePantry&apiKey=`
@@ -40,13 +40,13 @@ function generateRecipe(data){
     data.map(result => {
         generatedRecipe += 
         `
-        <div class="col s6 m3 xl2">
+        <div class="col s6 m4 xl3">
         <div class="recipe-card card">
           <div class="card-image waves-effect waves-block waves-light">
             <img class="img activator" src="${result.image}" alt="A picture of generated recipe">
           </div>
           <div class="card-content">
-            <span class="recipe-title card-title activator grey-text text-darken-4">${result.title}<i class="material-icons right"></i></span>
+            <span class="recipe-title card-title activator grey-text text-darken-4"git>${result.title}<i class="material-icons right"></i></span>
             <p><a href="#" data-recipe="${result.id}">Link to Recipe</a></p>
           </div>
           <div class="card-reveal">
@@ -60,16 +60,28 @@ function generateRecipe(data){
     recipeResults.innerHTML = generatedRecipe;
 }
 
+<<<<<<< HEAD
 // Sidenav
+=======
+Sidenav
+>>>>>>> 927fb32 (cleaned up elements in index, added switch for recipes generation)
 var instance = M.Sidenav.getInstance(elem);
 instance.open();
 instance.close();
 
+<<<<<<< HEAD
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
     // var collapsibleElem = document.querySelector('.collapsible');
     // var collapsibleInstance = M.Collapsible.init(collapsibleElem, {});
+=======
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+    var collapsibleElem = document.querySelector('.collapsible');
+    var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+>>>>>>> 927fb32 (cleaned up elements in index, added switch for recipes generation)
 });
         
