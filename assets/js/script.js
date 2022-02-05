24ceae3f6d071f81ab160d5567bd7697bd83259e
Andrew Ngo <94930359+MiinoSil@users.noreply.github.com>
2022-02-05 00:16:21 -0800

    reorganized Homepage HTML, hopefully cleaner, need to fix footer, now looks closer to figma

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,86 @@
 // Define all DOM elements here
 const testBtn = document.querySelector('.test-api')
-var ingredientSearchEl = document.querySelector('#searchBox')
+const recipeResults = document.querySelector('.recipe-results')
+const recipeNumber = document.querySelector('#recipe-number')
+
+// Searchbar elements
+const searchBTN = $('#searchBar');
+
+// const myIngCheck = document.querySelector('.myIngCheck')
+
 //API INFO HERE
 const APIKEY = '5f7f6407a3df426fb065f2211ab36e41'
 
 testBtn.addEventListener('click', getRecipe)
 
-
 function getRecipe() {
-    var ingredient = document.querySelector('.ing1').innerText
-    var recipeURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=`
-    + ingredient 
-    + `&number=3&apiKey=`
-    + APIKEY;
 
-    // console.log(recipeURL)
+    // if (myIngCheck.checked){
+    //     var myIngRank = "&ranking=2"
+    // }
+    // else {var myIngRank = ""}
+    // console.log(myIngRank)
+
+    var ingredients = document.querySelector('.ing1').innerText + "," + document.querySelector('.ing2').innerText + "," + document.querySelector('.ing3').innerText + ","  + document.querySelector('.ing4').innerText + ","
+    console.log(ingredients)
+    var recipeURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=`
+    + ingredients 
+    // + myIngRank
+    + `&number=`
+    + recipeNumber.value
+    + `&ignorePantry&apiKey=`
+    + APIKEY; 
+
+    console.log(recipeURL)
     fetch(recipeURL)
     .then((response) => response.json())
     // .then((data) => console.log(data))
-    .then((data) => showRecipes(data))
-
+    .then((data) => generateRecipe(data))
 }
 
-function showRecipes(data) {
-    let { title, image } = data[1]
-    let usedIng = data[1].usedIngredients[0].name
-    // console.log(usedIng)
-    console.log(title)
-    console.log(image)
-    document.querySelector('.r1img').src = image;
-    document.querySelector('.r1card-title').textContent = title;
-    document.querySelector('.r1currentRecipeIngredient').textContent = usedIng;
-
+function generateRecipe(data){
+    let generatedRecipe = '';
+    data.map(result => {
+        generatedRecipe += 
+        `
+        <div class="col s6 m3 xl2">
+        <div class="recipe-card card">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="img activator" src="${result.image}" alt="A picture of generated recipe">
+          </div>
+          <div class="card-content">
+            <span class="recipe-title card-title activator grey-text text-darken-4">${result.title}<i class="material-icons right"></i></span>
+            <p><a href="#" data-recipe="${result.id}">Link to Recipe</a></p>
+          </div>
+          <div class="card-reveal">
+            <span class="recipe-title card-title grey-text text-darken-4">${result.title}<i class="material-icons right">X</i></span>
+            <p class="currentRecipeIngredient">Missing Ingredients: <span class="missingIng">${result.missedIngredientCount}</span></p>
+          </div>
+        </div>
+    </div>
+     `  
+    })
+    recipeResults.innerHTML = generatedRecipe;
 }
 
-function handleIngSearch () {
-    var ingredientVal = document.querySelector('#search-input');
-    console.log('submit button works')
-    console.log(ingredientVal);
+function handleSearch{
+  var searchInputVal = $('#search').value
+  console.log(searchInputVal)
 }
 
-// Sidenav Element
-const slideMenu = document.querySelectorAll(".sidenav");
-M.Sidenav.init(slideMenu, {});
+// Sidenav
+var instance = M.Sidenav.getInstance(elem);
+instance.open();
+instance.close();
 
-// event listeners
-ingredientSearchEl.addEventListener('submit', handleIngSearch);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+    // var collapsibleElem = document.querySelector('.collapsible');
+    // var collapsibleInstance = M.Collapsible.init(collapsibleElem, {});
+});
+
+searchBTN.submit(handleSearch);
+
+        
