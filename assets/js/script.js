3fe277f8b0d0fe9cafd8d7412c2cab82b826e491
Rachel Marron <racheldmarron@gmail.com>
2022-02-05 11:53:25 -0800

    defined recipe java elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,3 +135,47 @@ function generateRecipe(data){
 // });
         
 init()
+
+
+
+
+/// JAVASCRIPT FOR RECIPE.HTML
+
+// Define all DOM elements here
+var getReturnBtn = document.querySelector('#returnBtn')
+const choiceRecipe = document.querySelector('#recipe-choice');
+const myIngredients = document.querySelector('#ingredients');
+const myTasteDescrip = document.querySelector('#taste-descrip');
+const myNutrition = document.querySelector("#nutritionFacts");
+const sourceUrl = document.querySelector('#sourceUrl'); 
+
+var recipes = [];
+
+
+// ?&apiKey=5f7f6407a3df426fb065f2211ab36e41
+
+
+// Event Listeners Here
+
+getReturnBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  window.history.go(-1); 
+  console.log("Check home button");
+  // close out html, return to main index 
+})
+
+
+// Image of recipe
+data.image 
+
+// Full ingredients by ID
+data.ingredientWidget
+
+// Taste description (i.e. sweet, spicy, bitter)
+data.tasteWidget
+
+// Nutrition facts by ID
+data.nutritionWidget
+
+// Link to view recipe source url 
+data.sourceUrl
