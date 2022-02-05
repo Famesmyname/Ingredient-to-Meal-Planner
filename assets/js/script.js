d28ebcdd60d3aa14502c7d0691eede1aa9e5d697
Edwin Fame <edwinfame@yahoo.com>
2022-02-04 21:32:57 -0800

    ingred list add remove functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Define all DOM elements here
-const getRecipeBtn = document.querySelector('.get-recipes')
-const recipeResults = document.querySelector('.recipe-results')
-const recipeNumber = document.querySelector('#recipe-number')
-const myIngCheck = document.querySelector('.myIngCheck')
+const getRecipeBtn = document.querySelector('#get-recipes')
+const recipeResults = document.querySelector('.recipe-results');
+const recipeNumber = document.querySelector('#recipe-number');
+const myIngCheck = document.querySelector('.myIngCheck');
 const ingredientInput = document.querySelector("#ing-text");
 const ingredientForm = document.querySelector("#ing-form");
 const ingredientList = document.querySelector("#ing-list");
@@ -10,19 +10,22 @@ const ingredientList = document.querySelector("#ing-list");
 var ingredients = [];
 
 //API INFO HERE
-const APIKEY = '5f7f6407a3df426fb065f2211ab36e41'
+// const APIKEY = '5f7f6407a3df426fb065f2211ab36e41'
+
 
 
 // Event Listeners Here
-getRecipeBtn.addEventListener('click', getRecipe)
+getRecipeBtn.addEventListener("click", function() {
+  console.log("Check button press")
+  getRecipe()
+})
 
 ingredientList.addEventListener("click", function(event) {
-  var element = event.target; 
-    var index = element.parentElement.getAttribute("data-index");
+    var element = event.target; 
+    var index = element.getAttribute("data-index");
     ingredients.splice(index, 1);
     storeIngs();
     renderIngs();
-
 });
 
 
@@ -49,6 +52,7 @@ function renderIngs() {
     li.setAttribute('data-index', i);
     li.classList.add('col')
     li.classList.add('s3')
+    li.classList.add('ing-item')
     ingredientList.appendChild(li);
   }
 }
@@ -67,22 +71,20 @@ function storeIngs() {
 
 
 function getRecipe() {
-
     if (myIngCheck.checked){
         var myIngRank = "&ranking=2"
     }
     else {var myIngRank = ""}
     console.log(myIngRank)
 
-    var ingredients = JSON.parse(localStorage.getItem("Ings"));
+    ingredients = JSON.parse(localStorage.getItem("Ings"));
     console.log(ingredients)
     var recipeURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=`
     + ingredients 
     + myIngRank
     + `&number=`
     + recipeNumber.value
-    + `&ignorePantry&apiKey=`
-    + APIKEY; 
+    + `&ignorePantry&apiKey=5f7f6407a3df426fb065f2211ab36e41`
 
     console.log(recipeURL)
     fetch(recipeURL)
@@ -101,13 +103,13 @@ function generateRecipe(data){
           <div class="card-image img-adjust waves-effect waves-block waves-light">
             <img class="img activator" src="${result.image}" alt="A picture of generated recipe">
           </div>
-          <div class="card-content flow-text">
+          <div class="name-card card-content flow-text">
             <span class="recipe-title card-title flow-text activator grey-text text-darken-4"git>${result.title}<i class="material-icons right"></i></span>
-            <p><a class="recipe-link" "href="#" data-recipe="${result.id}">Link to Recipe</a></p>
+            <p class="recipe-link"><a class="recipe-link" "href="#" data-recipe="${result.id}">Link to Recipe</a></p>
           </div>
-          <div class="card-reveal flow-text">
+          <div class="name-card card-reveal flow-text">
             <span class="recipe-title flow-text card-title grey-text text-darken-4">${result.title}<i class="material-icons right">X</i></span>
-            <p class="currentRecipeIngredient">Missing Ingredients: <span class="missingIng">${result.missedIngredientCount}</span></p>
+            <h5 class="currentRecipeIngredient">Missing Ingredients: <span class="missingIng">${result.missedIngredientCount}</span></h5>
           </div>
         </div>
     </div>
@@ -116,7 +118,7 @@ function generateRecipe(data){
     recipeResults.innerHTML = generatedRecipe;
 }
 
-// // Sidenav
+// Sidenav
 // var instance = M.Sidenav.getInstance(elem);
 // instance.open();
 // instance.close();
@@ -128,3 +130,4 @@ function generateRecipe(data){
 //     var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 // });
         
+init()
